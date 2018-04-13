3f9352b77d112cf0507c35e83d7d14fa1e39f336
Chuefeng Vang <Chewfungus209@gmail.com>
2018-04-12 22:07:39 -0700

    Special Stack implemented.
    Fixed Linked list conversion to the stack. Implemented a duplication function to duplicate all stack nodes to tree nodes. Needs conversion to other header and cpp files. Will begin documentation soon as well as a cleaner program. Will also implement private functions to allow a more secure program.

--- main.cpp
+++ main.cpp
@@ -1,114 +1,266 @@
-
 #include <iostream>
+
+
 
 using namespace std;
 
-struct Node{
-    Node();
-    Node(char input);
-  int frequency;
-  char character;
-  Node* right;
-  Node* left;
+
+
+class tnode{
+
+public:
+
+	tnode();
+
+	~tnode();
+
+
+
+	tnode* left;
+
+	tnode* right;
+
+	char character;
+
+	int frequency;
+
+
+
 };
 
-struct LinkedList{
-    LinkedList();
-    LinkedList(string str);
-    //~LinkedList;
-  Node* head = NULL;
-  void insert(char alpha);
-  void printf();
-  //void delete;
+
+
+class snode {
+
+public:
+
+	snode();
+	snode(char input);
+	~snode();
+
+
+
+	snode* next;
+
+	tnode* root=new tnode();
+
+	char character;
+
+	int frequency;
+
+
+
 };
 
-struct tree{
+class stack {
+public:
+    snode* head=NULL;
+    int size=0;
+
+    stack();
+    stack(string str);
+    //~stack();
+    snode* pop();               //Pop the top of stack.
+    void push(char alpha);      //Read from character, pushed onto the stack.
+    //void push(snode* input);    //Pushing the node into the stack. This is broken please change the variable.
+    void duplicate();
+    void sort();                //Quick Sort
+    void remove();
+    void printf();              //Test case.
+
+};
+
+class tree {
+
+public:
+
     tree();
-    //void insert(char alpha and maybe int frequency);
-    //~tree();
-    Node* root = NULL;
-};
 
-struct stack{
-    //stack();
-    //void getTop();
-    //Node* top = Null;
-    //Node* end = Null;
-    //void pop();
-    //void push();
+   // ~tree();
+
+    //void build(stack *list);
+
+
+
+    tnode* root=NULL;
+
 };
 
 int main()
+
 {   string in;
+
     cin >> in;
-    LinkedList newlist(in);
+
+    stack newlist(in);
+
+    newlist.duplicate();
     newlist.printf();
+    
     return 0;
+
 }
 
-LinkedList::LinkedList(){
+
+
+stack::stack(){
+
     head->character=NULL;
+
     head->frequency=0;
+
 }
 
-LinkedList::LinkedList(string str){
+
+
+stack::stack(string str){
+
     char store;
+
     for(int i=0; i<str.length(); i++){
+
         store=str[i];
-        insert(store);
+
+        push(store);
+
     }
+
 }
 
-void LinkedList::insert(char alpha){
+
+
+void stack::push(char alpha){
+
     if(!head){
-        head = new Node(alpha);
+
+        head = new snode(alpha);
+        size=1;
     }
-    
-    else if (head->character == alpha) {
-        head->frequency=head->frequency+1;
-    }
-    
+
     else {
-        Node* hold = head;
+
+        snode* hold = head;
+
         while(hold){
-            if(alpha==hold->character){
+
+            if(hold->character==alpha){
+
                 hold->frequency=hold->frequency+1;
+
                 return;
+
             }
+
             
-            else if(hold->right){
-            hold = hold->right;
+
+            else if(hold->next){
+
+            hold = hold->next;
+
             }
+
             else{
+
                 break;
+
             }
+
         }
-            Node* inserter = new Node(alpha);
-            hold->right=inserter;
+
+            snode* inserter = new snode(alpha);
+
+            hold->next=inserter;
+            size++;
+
     }
-}
-    
-void LinkedList::printf(){
-    Node* printer;
-    printer=head;
-    while(printer){
-        cout<<printer->character<<" "<<printer->frequency<<endl;
-        printer=printer->right;
-    }
+
 }
 
-Node::Node(){
-    character=NULL;
-    frequency=0;
-    right=NULL;
-    left=NULL;
+    
+
+void stack::printf(){
+
+    snode* printer;
+
+    printer=head;
+
+    while(printer){
+
+        cout<<printer->character<<" "<<printer->frequency<<endl;
+        cout<<printer->root->character<<" "<<printer->root->frequency<<endl;
+        printer=printer->next;
+
+    }
+
 }
-Node::Node(char input){
-    character=input;
-    frequency=1;
+
+void stack::duplicate(){
+
+    snode* printer;
+    printer=head;
+
+    while(printer){
+    //Duplicate all stack nodes to tree nodes;
+        printer->root->character = printer->character;
+        printer->root->frequency = printer->frequency;
+        printer=printer->next;
+    }
+
 }
 
 tree::tree(){
-    root->character=NULL;
-    root->frequency=0;
+
+	root=NULL;
+
+}
+
+
+
+/*tree::~tree() {
+
+	delete root;
+
+}*/
+
+
+
+/*void tree::build(stack *list){
+
+	snode *left,*right,*input;
+
+	    while(list->size>1){
+
+	        left=list->pop();
+
+	        right=list->pop();
+
+	        input=new snode;
+
+	        input->root=new tnode;
+
+	        input->root->left=left->root;
+
+	        input->root->right=right->root;
+
+	        input->frequency=left->frequency+right->frequency;
+
+	        input->root->frequency=input->frequency;
+
+	        list->push(input);
+
+	    }
+
+	    root=list->head->root;
+
+}*/
+
+snode::snode(char input) {
+    character = input;
+    frequency=1;
+    next=NULL;
+}
+
+tnode::tnode() {
+    character = NULL;
+    frequency = 0;
 }
